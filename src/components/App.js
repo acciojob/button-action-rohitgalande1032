451b0f7c3e4a4f276995c844f6989ded9abcb95b
rohitgalande1032 <rohitg3234@gmail.com>
2025-02-11 11:58:18 +0530

    add flag to show class dynamically e=whe click on buttton

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import './../styles/App.css';
 
-const App = (props) => {
+const App = () => {
 
+  const [flag, setFlag] = useState(false);
+  const handleClick = () => {
+    setFlag(true);  
+  }
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      <p id="para" className={flag ? "show" : "hide" }>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      <button onClick={handleClick} id="click">Click</button>
     </div>
   );
 }
